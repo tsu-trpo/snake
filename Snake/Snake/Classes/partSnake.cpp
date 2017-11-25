42b8bff362158a77c3a812304d32d12d900f646b
TarataichenkoDmitri <mrdrfunnyman@gmail.com>
2017-11-25 18:00:55 +0700

    cocos2dx only

--- Snake/Snake/Classes/partSnake.cpp
+++
@@ -1,9 +0,0 @@
-#include "partSnake.h"
-
-partSnake* partSnake::create(std::string name)
-{
-    partSnake *self = NULL;
-    self = new partSnake();
-    self->initWithFile(name);
-    return self;
-}
